b81c84ea05b9d7ca02a0cd5885ef86f1de5fd7c9
Jdoink <140544769+Jdoink@users.noreply.github.com>
2025-03-10 13:36:21 -0500

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="casino-container">
+      <h1>🎰 BET BET BET 🎰</h1>
+      {/* Slot machine UI will auto-render */}
+    </div>
+  )
+}
